ddae49f39417def3bf473b87613562231a036d2d
akaHendrik <akahendrik.info@gmail.com>
2024-04-24 00:58:46 +0200

    HN

---
+++ Eigene-Übungen/CRM/CMRv1/CMRv1/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CMRv1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string safename = "CRM";
+        private readonly string safepasswort = "0";
+
+        private string name;
+        private string passwort;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Abfrage vom eingegebenen Name
+            name = name_box.Text;
+
+            //Abfrage vom eingegebenen Passwort
+            passwort = passwort_box.Password;
+
+            //wenn Name und Passwort richtig dann weiterleitung zum nächsten Fenster
+            if (name == safename && passwort == safepasswort)
+            {
+                SecondWindow secondwindow = new SecondWindow();
+                this.Close();
+                secondwindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Ungültige eingabe!");
+            }
+        }
+
+        private void BeendenLogin_Click(object sender, RoutedEventArgs e)
+        {
+            //Abfrage beim Beenden des Programmes ob es wirklich geschlossen werden soll
+
+            if (MessageBox.Show("Sicher?", "Programm beenden", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                Environment.Exit(0);
+            }
+            //Funktioniert nicht
+            else
+            {
+                //SecondWindow secondWindow = new SecondWindow();
+                //secondWindow.Show();
+            }
+
+        }
+    }
+}
---
+++ Eigene-Übungen/CRM/CMRv1/CMRv1/SecondWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CMRv1
+{
+    /// <summary>
+    /// Interaktionslogik für SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnKundenverwaltungClick(object sender, RoutedEventArgs e)
+        {
+            BTNKunden.Content = new PageKunden();
+           
+            
+        }
+    }
+}
